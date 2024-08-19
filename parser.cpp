37df689130512f25b0324232cd36f40d54357e77
sergey <sboreysha@cognitech.su>
2024-08-19 17:07:41 +0300

    analitic

--- parser.cpp
+++ parser.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstring>
 #include <vector>
 #include <map>
 #include <iomanip>
+#include <sstream>
 
 using namespace std;
 //using namespace ofstream;
@@ -18,6 +20,66 @@ unsigned char hstob(char data)
         return (unsigned char)(data-87);
 
     return 0;
+}
+
+void print_sort(map<unsigned int, vector<vector<unsigned char>>> & data)
+{
+    for(pair plspair : data)
+    {
+        cout << "size " << plspair.first << " | contains objects " << plspair.second.size() << endl;
+    }
+}
+
+void print_group(map<unsigned int, vector<vector<unsigned char>>> & data, unsigned char key)
+{
+    vector<vector<unsigned char>> & outer_vec = data[key];
+
+    // Вывод элементов векторов  
+    for (const auto & inner_vec : outer_vec) 
+    {  
+        for (unsigned char value : inner_vec) {  
+            cout << hex << setw(2) <<static_cast<int>(value) << " " << dec;  
+        }  
+        cout << endl;  
+    }
+}
+
+void analitic(map<unsigned int, vector<vector<unsigned char>>> & data, unsigned char key)
+{
+    cout << ">>>> analitic..." << endl;
+
+    vector<vector<unsigned char>> & outer_vec = data[key];
+
+    vector<unsigned int> differences;
+
+    //...
+    for(unsigned int i=0;i<key;i++)
+    {
+        unsigned char temp = data[key][0][i];
+
+        for(vector<unsigned char> & inner_vec : outer_vec) 
+        {  
+            if(temp!=inner_vec[i])
+                differences.push_back(i);
+        }
+    }
+
+    if(differences.size())
+    {
+        cout << "found " << differences.size() << " differences" << endl;
+        cout << "are different ";
+
+        for(unsigned tmp : differences)
+        {
+            cout << dec << tmp << " ";
+        }
+
+        cout << "bytes" << endl;
+    }
+    else
+        {
+            cout << ">>>> payload equial!" << endl;
+        }
 }
 
 int main()
@@ -70,7 +132,7 @@ int main()
 
         for(size_t i=0;i<strtmp.size();i+=2)
         {
-            unsigned char _hex = hstob(strtmp[i])*16 + hstob(strtmp[i+1]);
+            unsigned char _hex = (unsigned char)(hstob(strtmp[i])*16 + hstob(strtmp[i+1]));
             temp.push_back(_hex);
         }
         
@@ -79,38 +141,96 @@ int main()
 
     cout << "convert " << index << " string..." << endl;
 
-    //сортировка
-    map<unsigned int, unsigned int> pltype;
+    //сортировка...
+    map<unsigned int, vector<vector<unsigned char>>> payload_sort;
 
-    for(vector<unsigned char> strtmp : payload_hex)
+    for(vector<unsigned char> uchtmp : payload_hex)
     {
-        unsigned int size = (unsigned int)strtmp.size();
+        unsigned int size = (unsigned int)uchtmp.size()-1;
 
-        if(pltype.count(size))
+        if(payload_sort.count(size))
         {
-            pltype[size] += 1;
+            payload_sort[size].push_back(uchtmp);
         }
         else
             {
-                pltype.insert(make_pair(size,1));
+                // Используем emplace для создания ключа и первоначального значения
+                //без make_pair!
+                payload_sort.emplace(size, vector<vector<unsigned char>>());  
+    
+                // Добавляем вектор в значение по ключу 1  
+                payload_sort[size].push_back(uchtmp);  
             }
     }
 
-    cout << "sorted " << pltype.size() << " uniq elements..." << endl;
+    cout << "sorted " << payload_sort.size() << " uniq elements..." << endl;
 
-     for(pair tmppr : pltype)
+    /*
+    *   командный блок...
+    */
+
+    string cmd;
+    string arg1(10,'\0');
+    string arg2(10,'\0');
+    
+    while(1)
     {
-        cout << "find " << tmppr.second << " pkg whis payload size " << tmppr.first << endl;
+        cmd.clear();
+        getline(cin, cmd);
+
+        sscanf(cmd.c_str(),"%s %s",arg1.data(),arg2.data());
+        unsigned int key = (unsigned int)atoi(arg2.c_str());
+
+        //печать группы пакетов...    
+        if(!memcmp(arg1.c_str(),"prints",6)||!memcmp(arg1.c_str(),"ps",2))
+        {
+            if(key>0)
+            {
+                if(payload_sort.count(key))
+                {
+                    print_group(payload_sort,(unsigned char)key);
+                    continue;
+                }
+                else
+                    {
+                        cout << ">>>> illegal key!" << endl;
+                        continue;
+                    }
+            }
+        }
+
+        //анализ группы пакетов...    
+        if(!memcmp(arg1.c_str(),"analitic",8)||!memcmp(arg1.c_str(),"al",2))
+        {
+            analitic(payload_sort,(unsigned char)key);
+            continue;
+        }
+
+        //печать списка сортировки...
+        if(cmd=="print"||cmd=="p")
+        {
+            print_sort(payload_sort);
+            continue;
+        }
+
+        //выход...
+        if(cmd=="quit"||cmd=="q")
+        {
+            break;
+        }
+
+        cout << ">>>> illegal cmd!" << endl;
+
     }
-
-
-
 
     //закрываем файл
     fin.close();
 }
-        /* for(unsigned char c : temp)
-        {
-            printf("%2x ",(unsigned char)c);
-        }
-        cout << endl; */
+
+// pair<const unsigned int, vector<unsigned char>> //для пары из map
+
+/* for(unsigned char c : temp)
+{
+    printf("%2x ",(unsigned char)c);
+}
+cout << endl; */
